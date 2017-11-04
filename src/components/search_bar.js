3c1e61ba69ba39108e1fb8f123716c1520334a23
Kevin Yeh <kevinyehdev@gmail.com>
2017-11-04 13:12:19 -0700

    build

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -12,6 +12,7 @@ class SearchBar extends Component {
       <div className="search-bar">
         <input
           value={this.state.term}
+          placeholder="Search query..."
           onChange={event => this.onInputChange(event.target.value)}
         />
       </div>
